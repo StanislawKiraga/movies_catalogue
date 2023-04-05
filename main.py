bd268f0db0086da2ad85159b03dcc46088eb5757
StanislawKiraga <kiragasta@gmail.com>
2023-04-05 13:19:49 +0200

    ...

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+import tmdb_client
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = [movie for movie in range(4)]
-    return render_template('homepage.htm', movies=movies)
+    movies = tmdb_client.get_movies(how_many=8)
+    return render_template("homepage.htm", movies=movies)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
 
 if __name__ == '__main__':
     app.run(debug=True)
