90096857518bff2ce1211c488c91503f383cd07f
StanislawKiraga <kiragasta@gmail.com>
2023-05-08 12:13:54 +0200

    add test homepage

--- test_tmdb.py
+++ test_tmdb.py
@@ -1,5 +1,7 @@
+import pytest
 import tmdb_client
 from unittest.mock import Mock
+from main import app
 
 def test_get_movies_list(monkeypatch):
    # Lista, którą będzie zwracać przysłonięte "zapytanie do API"
@@ -48,4 +50,19 @@ def test_get_single_movie_cast(monkeypatch):
    monkeypatch.setattr('tmdb_client.requests.get', request_mock)
    cast = tmdb_client.get_single_movie_cast(movie_id=1)
    assert cast == mock_cast
+
+@pytest.mark.parametrize('list_type', [
+   'popular',
+   'top_rated', 
+   'now_playing', 
+   'upcoming'
+   ])
+def test_homepage(monkeypatch, list_type):
+    api_mock = Mock(return_value={'results': []})
+    monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
+
+    with app.test_client() as client:
+        response = client.get(f'/?list_type={list_type}')
+        assert response.status_code == 200
+        api_mock.assert_called_with(f'movie/{list_type}')
 
