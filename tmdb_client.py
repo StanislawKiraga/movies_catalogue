3f58d1d25ff3ac7665663dbedd744d695c986f46
StanislawKiraga <kiragasta@gmail.com>
2023-05-08 12:45:37 +0200

    export API_TOKEN

--- tmdb_client.py
+++ tmdb_client.py
@@ -1,17 +1,21 @@
 import requests
 import random
+import os
 
-API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYWZmNDgzMGViMDY4NDRhNWIwMWQyMDNjYzJhNzkyNiIsInN1YiI6IjY0MmMzMjM3OGRlMGFlMDExMzUxMWEzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.w8oqRme7VJA9ccPaCAlXXrIN68m2jZl5vN-nsTDGzPc'
+API_TOKEN = os.environ.get('TMDB_API_TOKEN', '')
 
+def call_tmdb_api(endpoint):
+   full_url = f"https://api.themoviedb.org/3/{endpoint}"
+   headers = {
+       "Authorization": f"Bearer {API_TOKEN}"
+   }
+   response = requests.get(full_url, headers=headers)
+   response.raise_for_status()
+   return response.json()
 
 def get_movies_list(list_type):
-    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    response.raise_for_status()
-    return response.json()
+    return call_tmdb_api(f'movie/{list_type}')
+   
 
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
@@ -32,25 +36,13 @@ def get_movies(how_many, list_type):
     return data['results'][:how_many]
 
 def get_single_movie(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
+    return call_tmdb_api(f'movie/{movie_id}')
+    
 
 def get_single_movie_cast(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()["cast"]
+    return call_tmdb_api(f'movie/{movie_id}/credits')['cast']
+   
 
 def get_movie_images(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
-    headers = {
-        "Authorization": f"Bearer {API_TOKEN}"
-    }
-    response = requests.get(endpoint, headers=headers)
-    return response.json()
+    return call_tmdb_api(f'movie/{movie_id}/images')
+    
